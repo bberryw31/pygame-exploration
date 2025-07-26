3383d733961fbde2fb619b14f1a81de19d3ee733
JIMMY CHO <bberryw31@gmail.com>
2025-07-25 21:26:22 -0700

    Initiate Space Shooter

---
+++ space shooter/main.py
@@ -0,0 +1,65 @@
+import pygame
+import random
+
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+clock = pygame.time.Clock()
+
+# plain surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# importing an image
+player_surf = pygame.image.load('images/player.png').convert_alpha()
+player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(-1, 1)
+player_speed = 300
+
+star_surf = pygame.image.load('images/star.png').convert_alpha()
+star_positions = [(random.randint(0, WINDOW_WIDTH), random.randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+meteor_surf = pygame.image.load('images/meteor.png').convert_alpha()
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load('images/laser.png').convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+
+while running:
+    # delta time
+    dt = clock.tick() / 1000
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    # fill the window with color
+    display_surface.fill('darkgray')
+
+    # stars
+    for star_pos in star_positions:
+        display_surface.blit(star_surf, star_pos)
+
+    # player
+    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
+        player_direction.x = -player_direction.x
+        print(player_direction)
+    elif player_rect.bottom > WINDOW_HEIGHT or player_rect.top < 0:
+        player_direction.y = -1
+        print(player_direction)
+
+    player_rect.center += player_direction * player_speed * dt
+
+    # display
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+    display_surface.blit(player_surf, player_rect)
+    pygame.display.update()
+
+pygame.quit()
