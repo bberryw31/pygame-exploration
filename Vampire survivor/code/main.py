d2de27cc0bb2fef4d55aee3fa634767d6f4b8f2d
JIMMY CHO <bberryw31@gmail.com>
2025-08-14 19:27:59 -0700

    Initialize game class

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,38 +1,34 @@
 from settings import *
 
-# player
-class Player(pygame.sprite.Sprite):
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load("../images/player/down/0.png").convert_alpha()
-        self.rect = self.image.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Survivor")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+
+            #draw
+            pygame.display.update()
+
+        pygame.quit()
 
 
-# initiate
-pygame.init()
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Vampire Survivor")
-clock = pygame.time.Clock()
-running = True
+if __name__ == "__main__":
+    # run game
+    game = Game()
+    game.run()
 
-# imports
-
-
-# groups
-all_sprites = pygame.sprite.Group()
-player = Player(all_sprites)
-
-while running:
-    # dt
-    dt = clock.tick() / 1000
-
-    # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    display_surface.fill('gray')
-    all_sprites.draw(display_surface)
-    display_surface.blit(player.image, player.rect)
-
-pygame.quit()
